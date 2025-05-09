ea83ece898f19413a7fa5ecbabbbd87de82ee94b
yujintong <yujintong@126.com>
2025-05-10 00:20:28 +0800

    更新 docker-entrypoint.sh 文件 删除 环境变量文件

--- bird2/docker-entrypoint.sh
+++ bird2/docker-entrypoint.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-set -eu
-
-BIRD_RUN_DIR=/run/bird
+set -e
 
 if [ "$1" = 'bird' ]; then
+
+    BIRD_RUN_DIR=/run/bird
+    mkdir --parents "$BIRD_RUN_DIR";
+
+    # 配置文件目录
+    BIRD_CONF_DIR=/etc/bird
+    mkdir --parents "$BIRD_CONF_DIR";
+
+    # 日志目录
+    BIRD_LOG_DIR=/var/log/bird
+    mkdir --parents "$BIRD_LOG_DIR";
+
+    # 服务运行用户及用户组
+    BIRD_RUN_USER=bird
+    BIRD_RUN_GROUP=bird
+
     if [ ! -f "/etc/bird/bird.conf" ]; then
-        #mkdir -p "$BIRD_CONF_DIR";
-        #mkdir -p "$BIRD_LOG_DIR";
-        #mkdir -p "$BIRD_RUN_DIR";
-        cp -vf /usr/share/bird3/bird.conf /etc/bird
-        cp -vf /usr/share/bird3/envvars /etc/bird
+        cp -vf /usr/share/bird2/bird.conf /etc/bird
     fi
 
-    # 引入环境变量
-    . /etc/bird/envvars
-
+    chown -R --silent "$BIRD_RUN_USER:$BIRD_RUN_GROUP" "$BIRD_RUN_DIR"
     chown -R --silent "$BIRD_RUN_USER:$BIRD_RUN_GROUP" "$BIRD_CONF_DIR"
     chown -R --silent "$BIRD_RUN_USER:$BIRD_RUN_GROUP" "$BIRD_LOG_DIR"
-    chown -R --silent "$BIRD_RUN_USER:$BIRD_RUN_GROUP" "$BIRD_RUN_DIR"
     chmod 775 "$BIRD_RUN_DIR"
 
     if [ -d /docker-entrypoint.d ]; then
@@ -26,7 +33,13 @@ if [ "$1" = 'bird' ]; then
             [ -f "$f" ] && . "$f"
         done
     fi
-    exec /usr/sbin/bird -f -u $BIRD_RUN_USER -g $BIRD_RUN_GROUP $BIRD_ARGS
+
+    if [ -n "$BIRD_ARGS" ]; then
+        exec /usr/sbin/bird -f -u $BIRD_RUN_USER -g $BIRD_RUN_GROUP $BIRD_ARGS
+    else
+        exec /usr/sbin/bird -f -u $BIRD_RUN_USER -g $BIRD_RUN_GROUP
+    fi
+
 fi
 
 exec "$@"
